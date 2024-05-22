899a102542cf5a2109c244771f312db0968399b8
2012Inga <inganathi2001@gmail.com>
2024-05-22 06:02:52 -0700

    check-error

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,49 @@
-import React from 'react';
-import NavBar from './components/NavBar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const App = () => (
-  <Router>
-    <NavBar />
-    <div className="container mt-4">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleLogin = () => {
+    axios.post('http://localhost:3000/login', { username, password })
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        setMessage(error.response.data.message);
+      });
+  };
+
+  const handleRegister = () => {
+    axios.post('http://localhost:3000/register', { username, password })
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        setMessage(error.response.data.message);
+      });
+  };
+
+  return (
+    <div className="container">
+      <h1>Welcome to Community Connect</h1>
+      <div>
+        <h2>Login</h2>
+        <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
+        <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+        <button onClick={handleLogin}>Login</button>
+      </div>
+      <div>
+        <h2>Register</h2>
+        <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
+        <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+        <button onClick={handleRegister}>Register</button>
+      </div>
+      {message && <p>{message}</p>}
     </div>
-  </Router>
-);
+  );
+};
 
 export default App;
